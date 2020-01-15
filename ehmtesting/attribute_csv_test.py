0097e378712539c718627afc8b139646f67e2cc9
Adam Pumphrey <35270715+adampumphrey@users.noreply.github.com>
2020-01-14 22:57:38 -0700

    Fixed position formatting

--- ehmtesting/attribute_csv_test.py
+++ ehmtesting/attribute_csv_test.py
@@ -32,9 +32,23 @@ def parse_data(file):
     for row in players:
         # delete blank pair at end
         del row['']
+        # fix position formatting in output
+        if ',' in row['Position(s)']:
+            temp = row['Position(s)']
+            newtemp = ''.join(temp.split())
+            row['Position(s)'] = newtemp
+
+        # backup condition for position formatting
+        # if len(row['Position(s)']) > 2:
+        #     print(True)
+
         playerdata.append(row)
-    for item in playerdata[0]:
-        print(item, playerdata[0][item])
+
+    # testing
+    for i in range(len(playerdata)):
+        for item in playerdata[i]:
+            print(item, playerdata[i][item])
+    # /testing
 
     csvfile.close()
 
