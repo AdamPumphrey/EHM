2574c5ade018dfde56396e597619f23de9b52c05
Adam Pumphrey <35270715+adampumphrey@users.noreply.github.com>
2020-01-16 19:00:54 -0700

    Added encoding parameter to hopefully resolve future issues. Updated values to coincide with added Year column.

--- ehmtesting/stats_csv_parse.py
+++ ehmtesting/stats_csv_parse.py
@@ -11,34 +11,37 @@ playoffgoalies_filename = 'playoff_goalstatimport.csv'
 
 
 def format_skaters(skaters_name):
-    tempfile = open(skaters_name, 'r')
+    tempfile = open(skaters_name, 'r', encoding='cp1252')
     tempdata = tempfile.readlines()
 
-    tempdata[0] = tempdata[0][0:74] + '%' + tempdata[0][75:]
-    tempdata[0] = tempdata[0][0:134] + tempdata[0][135:]
+    # tempdata[0] = tempdata[0][0:74] + '%' + tempdata[0][75:]
+    tempdata[0] = tempdata[0][0:79] + '%' + tempdata[0][79:]
+    # tempdata[0] = tempdata[0][0:134] + tempdata[0][135:]
+    tempdata[0] = tempdata[0][0:139] + tempdata[0][140:]
     print(tempdata)
     tempfile.close()
 
-    tempfile = open(skaters_name, 'w')
+    tempfile = open(skaters_name, 'w', encoding='cp1252')
     tempfile.writelines(tempdata)
     tempfile.close()
 
 
 def format_goalies(goalies_name):
-    tempfile = open(goalies_name, 'r')
+    tempfile = open(goalies_name, 'r', encoding='cp1252')
     tempdata = tempfile.readlines()
 
-    tempdata[0] = tempdata[0][0:36] + '%' + tempdata[0][36:]
+    # tempdata[0] = tempdata[0][0:36] + '%' + tempdata[0][36:]
+    tempdata[0] = tempdata[0][0:41] + '%' + tempdata[0][41:]
     print(tempdata)
     tempfile.close()
 
-    tempfile = open(goalies_name, 'w')
+    tempfile = open(goalies_name, 'w', encoding='cp1252')
     tempfile.writelines(tempdata)
     tempfile.close()
 
 
 def parse_skaters(skaters_name):
-    skaters = open(skaters_name)
+    skaters = open(skaters_name, 'r', encoding='cp1252')
     playerstats = csv.DictReader(skaters, delimiter=',')
     playerstat_data = []
     for row in playerstats:
@@ -52,7 +55,7 @@ def parse_skaters(skaters_name):
 
 
 def parse_goalies(goalies_name):
-    goalies = open(goalies_name)
+    goalies = open(goalies_name, 'r', encoding='cp1252')
     goaliestats = csv.DictReader(goalies, delimiter=',')
     goaliestat_data = []
     for row in goaliestats:
