56a6f69738abcc2c0430af8eda07628e9366cdbf
Adam Pumphrey <35270715+adampumphrey@users.noreply.github.com>
2020-01-17 01:43:06 -0700

    Added some skeleton code, player table first-time importing

--- ehmtesting/ehmtracker.py
+++ ehmtesting/ehmtracker.py
@@ -4,6 +4,9 @@
 import stats_csv_test as statformat
 import stats_csv_parse as statparse
 import attribute_csv_test as attparse
+import sqlite3
+import database_config as db_config
+import os.path
 
 
 def get_statimport_files(statfile, teamid):
@@ -11,7 +14,7 @@ def get_statimport_files(statfile, teamid):
 
 
 def get_attimport_list(attfile, playeratts):
-    attparse.format_file(attfile)
+    # attparse.format_file(attfile)
     return attparse.parse_data(playeratts)
 
 
@@ -25,6 +28,53 @@ def get_goaliestat_list(goaliefile):
     return statparse.parse_goalies(goaliefile)
 
 
+def connection():
+    dbname = 'ehmtracking.db'
+    conn = None
+    try:
+        conn = sqlite3.connect(dbname)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+        return conn
+
+
+def create_db(conn):
+    db_config.create_player(conn)
+    db_config.create_player_attributes(conn)
+    db_config.create_regplayer_stats(conn)
+
+
+def import_player(conn, playeratts):
+    c = conn.cursor()
+    for row in playeratts:
+        c.execute("INSERT INTO player VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (row['Id'], row['Name'], row['Nation'],
+                                                                            row['Year'], row['Age'], row['Team Rights'],
+                                                                            row['Team'], row['League'],
+                                                                            row['Position(s)']))
+    conn.commit()
+
+
+def import_playeratts(conn, playeratts):
+    pass
+
+
+def import_skaterstats(conn, skaterstats):
+    pass
+
+
+def import_goaliestats(conn, goaliestats):
+    pass
+
+
+def import_poff_skaterstats(conn, skaterstats):
+    pass
+
+
+def import_poff_goaliestats(conn, goaliestats):
+    pass
+
+
 def main():
     attfile = 'testplayers.csv'
     statfile = 'teststats2.csv'
@@ -34,6 +84,19 @@ def main():
     reggoalies = 'regseason_goalstatimport.csv'
     poffskaters = 'playoff_statimport.csv'
     poffgoalies = 'playoff_goalstatimport.csv'
+    conn = connection()
+    get_statimport_files(statfile, teamid)
+    player_attlist = get_attimport_list(attfile, playeratts)
+    reg_skaterstats = get_skaterstat_list(regskaters)
+    reg_goaliestats = get_goaliestat_list(reggoalies)
+    poff_skaterstats = get_skaterstat_list(poffskaters)
+    poff_goaliestats = get_goaliestat_list(poffgoalies)
+    import_player(conn, player_attlist)
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
