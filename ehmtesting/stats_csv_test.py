56e15c9d833cac62f25e7bd0eaf95f7d5c3f47b3
Adam Pumphrey <35270715+adampumphrey@users.noreply.github.com>
2020-01-16 18:58:49 -0700

    Moved previous main() into function, added utility functions for changing filename and teamname, resolved TODO for formatting year

--- ehmtesting/stats_csv_test.py
+++ ehmtesting/stats_csv_test.py
@@ -2,37 +2,37 @@
 # EHM Assistant CSV parser for player stats
 # Opens and retrieves data from csv exported from EHM
 
-filename = 'teststats.csv'
-teamname = 'Edmonton'
+filename = 'teststats2.csv'
+teamname = 'Canucks'
 
 
-def parsedata(identifier, dataset, new_dataset):
-    loop = True
-    while loop:
-        if identifier in dataset[0]:
-            new_dataset.append(dataset[0])
-            del dataset[0]
-        elif 'Name,Team,Pos,' in dataset[0] or 'Playoffs' in dataset[0]:
-            loop = False
-        else:
-            del dataset[0]
-    return dataset, new_dataset
-
-
-def create_datafile(file_name, dataset):
-    datafile = open(file_name, 'w')
-    datafile.writelines(dataset)
-    datafile.close()
-
-
-def main():
-    stats = open(filename, 'r')
+def parse_statfile(filename, teamname):
+    stats = open(filename, 'r', encoding='cp1252')
     data = stats.readlines()
     stats.close()
 
-    # TODO: extract and insert year field for each stat row
+    year = int(data[0][41:43]) * 100
+    year2 = int(data[0][46:48])
+    if year2 == 0:
+        year += 1000
+    else:
+        year += year2
+    year = str(year) + ','
 
     del data[0:4]
+
+    for i in range(len(data)):
+        header = False
+        if 'Name,Team,Pos' in data[i]:
+            header = True
+        for x in range(len(data[i])):
+            if data[i][x] == ',':
+                if header:
+                    data[i] = data[i][:x + 1] + 'Year,' + data[i][x + 1:]
+                else:
+                    data[i] = data[i][:x + 1] + year + data[i][x + 1:]
+                break
+
     regseason_skaterdata = [data[0]]
     data.append(data[0])
     del data[0]
@@ -55,6 +55,39 @@ def main():
         del data[0]
         data, playoff_goaldata = parsedata(teamname, data, playoff_goaldata)
         create_datafile('playoff_goalstatimport.csv', playoff_goaldata)
+
+
+def parsedata(identifier, dataset, new_dataset):
+    loop = True
+    while loop:
+        if identifier in dataset[0]:
+            new_dataset.append(dataset[0])
+            del dataset[0]
+        elif 'Name,Year,Team,Pos,' in dataset[0] or 'Playoffs' in dataset[0]:
+            loop = False
+        else:
+            del dataset[0]
+    return dataset, new_dataset
+
+
+def create_datafile(file_name, dataset):
+    datafile = open(file_name, 'w', encoding='cp1252')
+    datafile.writelines(dataset)
+    datafile.close()
+
+
+def set_filename(newname):
+    filename = newname
+    return filename
+
+
+def set_teamname(newname):
+    teamname = newname
+    return teamname
+
+
+def main():
+    parse_statfile(filename, teamname)
 
 
 if __name__ == '__main__':
